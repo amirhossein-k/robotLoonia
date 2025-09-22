c9f644eb8e7bf4232b7e4897e8f72edbd2b43ee1
amirhossein-k <amirhossein.k787878@gmail.com>
2025-09-23 01:18:48 +0330

    uiokl;

--- app/telegram/bot.ts
+++ app/telegram/bot.ts
@@ -632,10 +632,10 @@ bot.action("end_chat", async (ctx) => {
 
     // اطلاع به هر دو طرف + بازگرداندن به پروفایل
     await ctx.reply("❌ شما چت را قطع کردید.");
-    await showProfile(user.telegramId, true); // کاربر
+    await showProfile(user.telegramId, false); // کاربر
 
     await ctx.telegram.sendMessage(chatWith, `❌ کاربر ${user.name} چت را قطع کرد.`);
-    await showProfile(chatWith, false); // ادمین
+    await showProfile(chatWith, true); // ادمین
 
 
 });
