43d59558d13abf0656d1f0f3d73bdaad255ba02f
amirhossein-k <amirhossein.k787878@gmail.com>
2025-09-22 23:53:46 +0330

    regegeg

--- app/model/Chat.ts
+++ app/model/Chat.ts
@@ -1,23 +1,26 @@
 // app/model/Chat.ts
 import mongoose, { Schema, Document } from "mongoose";
 
+interface IMessage {
+    from: number;
+    to: number;
+    text?: string;
+    photo?: string;
+    voice?: string;
+    type: "text" | "photo" | "voice";
+    createdAt: Date;
+}
 interface IChat extends Document {
-    users: number[]; // دو کاربر
+    users: number[];
     startedAt: Date;
     endedAt?: Date;
-    messages: {
-        from: number;
-        to: number;
-        text?: string;
-        photo?: string;
-        voice?: string;
-        type: "text" | "photo" | "voice";
-        createdAt: Date;
-    }[];
+    messages: IMessage[];
 }
 
+
+
 const ChatSchema = new Schema<IChat>({
-    users: { type: [Number], required: true },
+    users: { type: [Number], required: true, validate: [(val: number[]) => val.length === 2, 'چت باید دقیقاً 2 کاربر داشته باشد'] },
     startedAt: { type: Date, default: Date.now },
     endedAt: { type: Date },
     messages: [
@@ -27,10 +30,9 @@ const ChatSchema = new Schema<IChat>({
             text: String,
             photo: String,
             voice: String,
-            type: String,
+            type: { type: String, enum: ["text", "photo", "voice"], required: true },
             createdAt: { type: Date, default: Date.now },
         },
     ],
 });
-
 export default mongoose.models.Chat || mongoose.model<IChat>("Chat", ChatSchema);
